75fbf015d20aaf6b4590b5816f7929894459f9b7
gcnyin <guchounongyin@gmail.com>
2020-11-10 00:05:53 +0800

    add logging; using coroutines

--- src/main/kotlin/com/github/gcnyin/ktstarter/App.kt
+++ src/main/kotlin/com/github/gcnyin/ktstarter/App.kt
@@ -2,11 +2,14 @@ package com.github.gcnyin.ktstarter
 
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
+import io.vertx.kotlin.coroutines.dispatcher
 import io.vertx.mysqlclient.MySQLConnectOptions
 import io.vertx.mysqlclient.MySQLPool
 import io.vertx.sqlclient.PoolOptions
+import mu.KotlinLogging
 
 fun main(args: Array<String>) {
+  val logger = KotlinLogging.logger {}
   val vertx = Vertx.vertx()
   val connectOptions = MySQLConnectOptions().setPort(3306).setHost("localhost")
     .setDatabase("testdb").setUser("admin").setPassword("password")
@@ -14,10 +17,12 @@ fun main(args: Array<String>) {
   val mySQLPool = MySQLPool.pool(connectOptions, poolOptions)
 
   val router = Router.router(vertx)
-  router.get("/user/:userId").handler(UserQueryHandler(mySQLPool))
+  router.get("/user/:userId").handler(UserQueryHandler(mySQLPool, vertx.dispatcher()))
   router.errorHandler(500) { ctx ->
     ctx.response().end("error")
   }
   val httpServer = vertx.createHttpServer()
-  httpServer.requestHandler(router).listen(8080)
+  httpServer.requestHandler(router).listen(8080) { r ->
+    logger.info { "listen on 8080 port" }
+  }
 }
--- src/main/kotlin/com/github/gcnyin/ktstarter/UserQueryHandler.kt
+++ src/main/kotlin/com/github/gcnyin/ktstarter/UserQueryHandler.kt
@@ -2,9 +2,16 @@ package com.github.gcnyin.ktstarter
 
 import io.vertx.core.Handler
 import io.vertx.ext.web.RoutingContext
+import io.vertx.kotlin.coroutines.await
 import io.vertx.mysqlclient.MySQLPool
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import mu.KotlinLogging
+import kotlin.coroutines.CoroutineContext
 
-class UserQueryHandler(var mySQLPool: MySQLPool) : Handler<RoutingContext> {
+class UserQueryHandler(private val mySQLPool: MySQLPool, override val coroutineContext: CoroutineContext) : Handler<RoutingContext>, CoroutineScope {
+  private val logger = KotlinLogging.logger {}
+
   override fun handle(ctx: RoutingContext?) {
     if (ctx == null) {
       return
@@ -14,27 +21,27 @@ class UserQueryHandler(var mySQLPool: MySQLPool) : Handler<RoutingContext> {
       ctx.response().end("Invalid userId");
       return
     }
-    mySQLPool
-      .query("select id, name from user where id = $userId")
-      .execute { ar ->
-        if (ar.succeeded()) {
-          val rows = ar.result()
-          if (rows.size() < 1) {
-            ctx.response().setStatusCode(404).end("user $userId not found")
-            return@execute
-          }
-          val row = rows.elementAt(0)
-          val id = row?.getInteger(0)
-          val name = row?.getString(1)
-          val user = id?.let { name?.let { it1 -> User(it, it1) } }
-          val userString = mapper.writeValueAsString(user)
-          val response = ctx.response()
-          response.putHeader("content-type", "application/json")
-          response.end(userString)
-        } else {
-          ar.cause().printStackTrace();
-          ctx.response().setStatusCode(500).end("error")
+    launch {
+      try {
+        val rows = mySQLPool.query("select id, name from user where id = '$userId'").execute().await()
+        if (rows.size() < 1) {
+          val s = "user $userId not found"
+          logger.info { s }
+          ctx.response().setStatusCode(404).end(s)
+          return@launch
         }
+        val row = rows.elementAt(0)
+        val id = row?.getInteger(0)
+        val name = row?.getString(1)
+        val user = id?.let { name?.let { it1 -> User(it, it1) } }
+        val userString = mapper.writeValueAsString(user)
+        val response = ctx.response()
+        response.putHeader("content-type", "application/json")
+        response.end(userString)
+      } catch (e: Exception) {
+        logger.error(e) { "Internal error" }
+        ctx.response().setStatusCode(500).end("Internal error")
       }
+    }
   }
 }
