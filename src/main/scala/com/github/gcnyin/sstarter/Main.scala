fe0eed7d165322ee3933a69ec9dce0c33726a88e
gcnyin <guchounongyin@gmail.com>
2020-11-16 18:55:11 +0800

    add HttpVerticle in scala example

--- src/main/scala/com/github/gcnyin/sstarter/Main.scala
+++ src/main/scala/com/github/gcnyin/sstarter/Main.scala
@@ -1,7 +1,8 @@
 package com.github.gcnyin.sstarter
 
 import com.github.gcnyin.sstarter.VertxFutureConverters.CompletionStageOps
-import io.vertx.core.Vertx
+import io.vertx.core.{AbstractVerticle, Handler, Promise, Vertx}
+import io.vertx.ext.web.{Router, RoutingContext}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
@@ -9,14 +10,35 @@ import scala.util.{Failure, Success}
 object Main {
   def main(args: Array[String]): Unit = {
     val vertx: Vertx = Vertx.vertx()
-    val future = vertx.createHttpServer().requestHandler(request => {
-      val response = request.response()
-      response.putHeader("content-type", "application/json")
-      response.`end`("""{"message":"ok"}""")
-    }).listen(8081).asScala
+    val future = vertx.deployVerticle(new HttpVerticle(8081)).asScala
     future onComplete {
       case Success(value) => println(value)
-      case Failure(exception) => println("An error has occurred: " + exception.getMessage)
+      case Failure(_) => vertx.close()
     }
+  }
+}
+
+class HttpVerticle(val port: Int) extends AbstractVerticle {
+
+  override def start(startPromise: Promise[Void]): Unit = {
+    val router = Router.router(vertx)
+    router.get("/info").handler(InfoGetHandler)
+    val future = vertx.createHttpServer().requestHandler(router).listen(port).asScala
+    future onComplete {
+      case Success(value) =>
+        println("verticle successfully deployed, " + value)
+        startPromise.complete()
+      case Failure(exception) =>
+        println("deployment failed: " + exception.getMessage)
+        startPromise.fail(exception)
+    }
+  }
+}
+
+object InfoGetHandler extends Handler[RoutingContext] {
+  override def handle(ctx: RoutingContext): Unit = {
+    val response = ctx.response()
+    response.putHeader("content-type", "application/json")
+    response.`end`("""{"message":"ok"}""")
   }
 }
